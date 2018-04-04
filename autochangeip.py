e77ad8a2020f0c56f0f6828fa87c57591eb76549
Hrvoje <hrvooje@gmail.com>
2018-04-04 15:11:21 +0200

    Added variable network address part 'ip_network'

--- autochangeip.py
+++ autochangeip.py
@@ -15,7 +15,6 @@ def is_admin():
         return False
 
 if is_admin():
-    input("admin sam. prije naredbe if petlja")
     # cmd shows network interface names (Local Area network, Ethernet, Wireless Network Connection)
     cmd_inter = ['netsh', 'interface', 'show', 'interface']
     # cmd_ip = ['netsh', 'interface', 'ip', 'show', 'config', 'name=Ethernet', '|', 'findstr', 'IP Address']
@@ -57,23 +56,30 @@ if is_admin():
     hostname = socket.gethostname()
     ip = socket.gethostbyname(hostname)
 
-    print(net_int)
-    print(ip)
+    # find out third octet from IP address
+    ip_list = ip.split('.')
+    ip_network = ip_list[2]
+
+    print('net_int: {}'.format(net_int))
+    print('ip: {}'.format(ip))
+    print('ip_list: {}'.format(ip_list))
+    print('ip_network: {}'.format(ip_network))
+
 
     # cmd_change_ip = ['netEsh', 'interface', 'ip', 'set', 'address',
     #                 net_int, 'static', '192.168.0.3', '255.255.255.0', '192.168.0.251', '1']
-    cmd_change_ip_raw = 'netsh interface ip set address' + ' ' + net_int + ' ' + 'static 192.168.0.3 255.255.255.0 192.168.0.251 1'
+
+    cmd_change_ip_raw = 'netsh interface ip set address' + ' ' + net_int + ' ' + \
+                        'static 192.168.' + ip_network + '.2' + ' ' + '255.255.255.0 192.168.0.251 1'
     cmd_change_ip = shlex.split(cmd_change_ip_raw)
 
     sp = subprocess.Popen(cmd_change_ip, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = sp.communicate()
     print(out, err, sp.returncode)
-    input("admin sam. poslije naredbe if petlja")
+    input("Enter for end")
 else:
-    input("nisam admin. prije naredbe else petlja")
     # Re-run the program with admin rights
     ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, sys.argv[0], None, 1)
-    input("nisam admin. poslije naredbe else petlja")
 
 
 
